96f97a1202873f1fe1f892e015fc47eb51aab4e1
displaynameroblox <a14111alomari@gmail.com>
2025-09-03 15:48:46 +0300

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,26 @@
+-- init.lua
+local BASE_URL = "https://raw.githubusercontent.com/"
+
+-- Simple require-from-web
+local function requireFromUrl(file)
+    local source = game:HttpGet(BASE_URL .. file)
+    return loadstring(source)()
+end
+
+-- Load modules
+local PlaybackManager = requireFromUrl("PlaybackManager.lua")
+local UIManager = requireFromUrl("UIManager.lua")
+local PlaylistManager = requireFromUrl("PlaylistManager.lua")
+local ThemeManager = requireFromUrl("ThemeManager.lua")
+
+-- Initialize system
+local MusicPlayer = {}
+MusicPlayer.Playback = PlaybackManager
+MusicPlayer.UI = UIManager
+MusicPlayer.Playlist = PlaylistManager
+MusicPlayer.Theme = ThemeManager
+
+-- Boot UI
+UIManager.init(MusicPlayer)
+
+return MusicPlayer
