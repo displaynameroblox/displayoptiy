21d49f05004f9063befb21f9125cdd0036fc8844
displaynameroblox <a14111alomari@gmail.com>
2025-09-05 13:50:27 +0300

    Update PlaybackManager.lua

--- PlaybackManager.lua
+++ PlaybackManager.lua
@@ -1,39 +1,79 @@
 -- PlaybackManager.lua
-local PlaybackManager = {
-    currentTrack = nil,
-    repeatMode = "off", -- "off", "one", "all"
-    shuffle = false,
-    volume = 0.5
-}
+return function(deps)
+    local Util = deps.Util
+    local PlaylistManager = deps.PlaylistManager
 
-function PlaybackManager:play(track)
-    if self.currentTrack then
-        self.currentTrack:Stop()
+    local PlaybackManager = {
+        currentSound = nil,
+        currentPlaylist = nil,
+        currentIndex = 0,
+    }
+
+    PlaybackManager.OnTrackChanged = Instance.new("BindableEvent").Event
+    PlaybackManager.OnPlayStateChanged = Instance.new("BindableEvent").Event
+
+    function PlaybackManager.play(playlistName, index)
+        local tracks = PlaylistManager.getTracks(playlistName)
+        if not tracks[index] then return end
+
+        if PlaybackManager.currentSound then
+            PlaybackManager.currentSound:Destroy()
+        end
+
+        local sound = Instance.new("Sound")
+        sound.SoundId = tracks[index].id
+        sound.Looped = false
+        sound.Parent = game:GetService("SoundService")
+        sound:Play()
+
+        PlaybackManager.currentSound = sound
+        PlaybackManager.currentPlaylist = playlistName
+        PlaybackManager.currentIndex = index
+
+        PlaybackManager.OnTrackChanged:Fire(tracks[index])
+        PlaybackManager.OnPlayStateChanged:Fire(true)
+
+        sound.Ended:Connect(function()
+            PlaybackManager.next()
+        end)
     end
-    self.currentTrack = Instance.new("Sound")
-    self.currentTrack.SoundId = track
-    self.currentTrack.Volume = self.volume
-    self.currentTrack.Parent = game:GetService("SoundService")
-    self.currentTrack:Play()
-end
 
-function PlaybackManager:pause()
-    if self.currentTrack then
-        self.currentTrack:Pause()
+    function PlaybackManager.pause()
+        if PlaybackManager.currentSound then
+            PlaybackManager.currentSound:Pause()
+            PlaybackManager.OnPlayStateChanged:Fire(false)
+        end
     end
-end
 
-function PlaybackManager:resume()
-    if self.currentTrack then
-        self.currentTrack:Resume()
+    function PlaybackManager.resume()
+        if PlaybackManager.currentSound then
+            PlaybackManager.currentSound:Resume()
+            PlaybackManager.OnPlayStateChanged:Fire(true)
+        end
     end
-end
 
-function PlaybackManager:setVolume(v)
-    self.volume = math.clamp(v, 0, 1)
-    if self.currentTrack then
-        self.currentTrack.Volume = self.volume
+    function PlaybackManager.stop()
+        if PlaybackManager.currentSound then
+            PlaybackManager.currentSound:Stop()
+            PlaybackManager.OnPlayStateChanged:Fire(false)
+        end
     end
-end
 
-return PlaybackManager
+    function PlaybackManager.next()
+        if not PlaybackManager.currentPlaylist then return end
+        local tracks = PlaylistManager.getTracks(PlaybackManager.currentPlaylist)
+        local newIndex = PlaybackManager.currentIndex + 1
+        if newIndex > #tracks then newIndex = 1 end
+        PlaybackManager.play(PlaybackManager.currentPlaylist, newIndex)
+    end
+
+    function PlaybackManager.prev()
+        if not PlaybackManager.currentPlaylist then return end
+        local tracks = PlaylistManager.getTracks(PlaybackManager.currentPlaylist)
+        local newIndex = PlaybackManager.currentIndex - 1
+        if newIndex < 1 then newIndex = #tracks end
+        PlaybackManager.play(PlaybackManager.currentPlaylist, newIndex)
+    end
+
+    return PlaybackManager
+end
